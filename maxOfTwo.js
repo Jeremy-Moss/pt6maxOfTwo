f8b2658e9803dc393a868e2f719d96e3f776a123
Jeremy-Moss <mosstoggle@gmail.com>
2022-08-24 01:22:28 -0400

    first commit(done)

---
+++ maxOfTwo.js
@@ -0,0 +1,11 @@
+let x = 9
+let y = 9
+
+// Write your code below this line.
+if(x > y){
+    console.log(x)
+} else if(y > x){
+    console.log(y)
+} else {
+    console.log('The values are identical')
+}
